b8fc49d653f06b8d7ea19d2059dd0f95cd7283e2
Kevin Bevan <kevin.bevan@cdsm.co.uk>
2020-07-14 02:31:26 +0100

    run prettier on quotes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import { Header } from './components/Header/Header'
-import { Trending } from './components/Trending/Trending'
-import { PopularList } from './components/Popular/Popular'
-import { MoviePage } from './pages/movie-page/MoviePage'
-import { HashRouter as Router, Route, Switch } from 'react-router-dom'
+import React from "react"
+import { Header } from "./components/Header/Header"
+import { Trending } from "./components/Trending/Trending"
+import { PopularList } from "./components/Popular/Popular"
+import { MoviePage } from "./pages/movie-page/MoviePage"
+import { HashRouter as Router, Route, Switch } from "react-router-dom"
 
 const App: React.FC = () => {
-    return (
-        <>
-            <Router>
-                <Header />
-                <Switch>
-                    <Route path="/movie/:id" component={MoviePage} />
-                    <div>
-                        <Trending />
-                        <PopularList />
-                    </div>
-                </Switch>
-            </Router>
-        </>
-    )
+	return (
+		<>
+			<Router>
+				<Header />
+				<Switch>
+					<Route path='/movie/:id' component={MoviePage} />
+					<div>
+						<Trending />
+						<PopularList />
+					</div>
+				</Switch>
+			</Router>
+		</>
+	)
 }
 
 export { App }
