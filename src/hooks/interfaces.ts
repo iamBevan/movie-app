b24f97c517589b6880f92cc03d4a56ad76e22879
Kevin Bevan <iambevan@gmail.com>
2020-11-03 03:26:56 +0000

    linting

--- src/hooks/interfaces.ts
+++ src/hooks/interfaces.ts
@@ -1,52 +1,52 @@
-export interface Data {
-    cast: Cast[]
-    crew: Crew[]
-    id: number
+export interface CastData {
+	cast: Cast[] | undefined;
+	crew: Crew[] | undefined;
+	id: number | undefined;
 }
 
 export interface Cast {
-    cast_id: number
-    character: string
-    credit_id: string
-    gender: number
-    id: number
-    name: string
-    order: number
-    profile_path: string
+	cast_id: number;
+	character: string;
+	credit_id: string;
+	gender: number;
+	id: number;
+	name: string;
+	order: number;
+	profile_path: string;
 }
 
 export interface Crew {}
 
 export interface KnownFor {
-    poster_path: string | null
-    adult: boolean
-    overview: string
-    release_date: string
-    original_title: string
-    genre_ids: number[]
-    id: number
-    media_type: string
-    original_language: string
-    title: string
-    backdrop_path: string | null
-    popularity: number
-    vote_count: number
-    video: boolean
-    vote_average: number
+	poster_path: string | null;
+	adult: boolean;
+	overview: string;
+	release_date: string;
+	original_title: string;
+	genre_ids: number[];
+	id: number;
+	media_type: string;
+	original_language: string;
+	title: string;
+	backdrop_path: string | null;
+	popularity: number;
+	vote_count: number;
+	video: boolean;
+	vote_average: number;
 }
 
 export interface PopularResults {
-    profile_path: string
-    adult: boolean
-    id: number
-    known_for: KnownFor
-    name: string
-    popularity: number
+	profile_path: string;
+	adult: boolean;
+	id: number;
+	known_for: KnownFor;
+	name: string;
+	popularity: number;
 }
 
 export interface Popular {
-    page: number
-    total_results: number
-    total_pages: number
-    results: PopularResults[]
+	page: number;
+	total_results: number;
+	total_pages: number;
+	results: PopularResults[];
 }
